b7c9b4e5b25b893a16a8aa7750eca2add7a9fd8d
acoffeerunner <acoffeerunner@gmail.com>
2018-10-01 18:27:42 +0530

    del: init Initialise del.cpp with the necessary function to delete student records.

---
+++ del.cpp
@@ -0,0 +1,88 @@
+#include "menu.cpp"
+void del_sr()
+{
+    //declare required int variables
+    int r;
+    int chk = 0;
+
+    //ifstream object for record file
+    ifstream sr_i;
+    sr_i.open("student_records.txt", ios : in);
+
+    //marks class object
+    marks m1;
+
+    roll_enter:
+    //roll number entering action begins here
+    cout << "\nEnter the roll number of the student record to be deleted: ";
+    cin >> r;
+    cout << "\nPlease wait while the records are analysed...";
+    cout << "\n|==========================";
+
+    //checking till eof for record with the entered roll number 
+    while (!sr_i.eof())
+    {
+        sr_i.read((char *)m1, sizeof(m1));
+        if (m1.rollchk() == 1)
+        {
+            chk = 1;
+            break;
+        }
+    }
+    
+    sleepfor(milliseconds(650));
+    cout << "================================|";
+
+    //if else for whether the record was found or not
+    if (chk == 1)
+    {
+        cout << "\nStudent record found!";
+        ofstream temp;
+        temp.open("temp.txt", ios : out);
+        sr_i.seekg(0, ios : beg);
+        temp.seekp(0, ios : beg);
+        
+        while (!sr1.eof())      
+        {
+            sr1.read((char *)m1, sizeof(m1));
+            if (m1.chkroll() == 0)
+            {
+                //copying records to temp.txt
+                temp.write((char *)m1, sizeof(m1));
+            }
+
+            //fstream objects closed
+            temp.close();
+            sr_i.close();
+
+            //remove rename file action here
+            remove("student_details.txt");
+            rename("temp.txt", "student_details.txt");
+        }
+    }
+    else
+    {
+        char k;
+        cout << "\nNo student with roll number " << r << " found!";
+        ta:
+        cout << "\nWould you like to try again? (Y/N)";
+        cin >> k;
+
+        if (k == 'n' || k == 'N')          //no for trying again
+        {
+            sr_i.close();
+            clrscr();
+            goto menu;
+        }
+        else if (k == 'Y' || k == 'y')     //yes for trying again
+        {
+            clrscr();
+            goto roll_enter;
+        }
+        else                               //invalid input
+        {
+            cout << "\nInvalid input. Try again with a correct input.";
+            goto ta;
+        }
+    }
+}
