a850bae2f4fd216c14d85062e3bc0d2e3d93c42f
acoffeerunner <acoffeerunner@gmail.com>
2018-10-01 14:43:47 +0530

    creation: beautify this code, m80s. Code cleanup and added required comments to better the workflow. Yes, great coding practices, I know.

--- creation.cpp
+++ creation.cpp
@@ -1,40 +1,53 @@
 #include "menu.cpp"
 void cr_sr()
 {
+    //ofstream object here
     ofstream sr;
-    sr.open("student_records.txt", ios:out);
+    sr.open("student_records.txt", ios : out);
+
+    //marks class object here
     marks m1;
     m1.get_info();
-    char l; 
+
+    //boring re-enter details part
+    char l;
     l_ladder:
-    cout<<"Would you like to re-enter the student record? (Y/N)   ";
-    cin>>l;
-    if(l=='y' || l=='Y')
+    cout << "Would you like to re-enter the student record? (Y/N)   ";
+    cin >> l;
+
+    //if else for l
+    if (l == 'y' || l == 'Y')           //when the user wants to re-enter
     {
         clrscr();
-        cout<<"Re-enter student details:\n";
+        cout << "Re-enter student details:\n";
         m1.get_info();
     }
-    else if(l=='n' || l=='N')
+
+    else if (l == 'n' || l == 'N')      //when the user doesn't want to re-enter
     {
         clrscr();
-        cout<<"Writing records to file...\n";
-        cout<<"|=========";
+
+        //ugly progress bar stuff
+        cout << "Writing records to file...\n";
+        cout << "|=========";
         sleepfor(milliseconds(600));
-        cout<<"========";
+        cout << "========";
         sleepfor(milliseconds(500));
-        cout<<"========|";
-        sr.write((char*)&m1, sizeof(m1));
-        cout<<"\nWrite operation successful!";
+        cout << "========|";
+        sr.write((char *)&m1, sizeof(m1));
+        cout << "\nWrite operation successful!";
         getchar();
-        cout<<"\nPlease wait while you're redirected to the main menu...";
+
+        //redirect to menu.cpp
+        cout << "\nPlease wait while you're redirected to the main menu...";
         sleepfor(seconds(5));
         clrscr();
         goto menu;
     }
-    else
-    {   
+    
+    else        //invalid case
+    {
         clrscr();
-        cout<<"Invalid option selected!\n";
+        cout << "Invalid option selected!\n";
         goto l_ladder;
-}
+    }
