c7d9d163e199cc1448d7c369f670cb08e312250c
acoffeerunner <acoffeerunner@gmail.com>
2018-10-02 16:08:31 +0530

    src: init Initialised file which displays all student records.

---
+++ src.cpp
@@ -0,0 +1,29 @@
+#ifndef includes
+#define includes
+//class_inc header
+#include "class_inc.h"
+//menu include
+#include "menu.cpp"
+#endif
+
+using namespace std;
+void disp_src()
+{
+    //ifstream object
+    ifstream sr_i;
+    sr_i.open("student_details.txt", ios::in);
+    
+    //get pointer to 0 position of file
+    sr_i.seekg(0, ios::beg);
+
+    //marks object
+    marks m1;
+    
+    while(!sr_i.eof())
+    {
+        sr_i.read((char *)&m1, sizeof(m1));
+        m1.disp_marks();
+        getchar();
+        cout<<"\n\n\n\n\n\n";
+    }
+}
